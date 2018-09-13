5660770915c379edb2f35e176496ed5eeaab8362
jeyakumar8181 <37777450+jeyakumar8181@users.noreply.github.com>
2018-09-13 18:39:56 +0530

    Update main.tf

--- module/ec2/main.tf
+++ module/ec2/main.tf
@@ -3,6 +3,7 @@ provider "aws" {
   secret_key = "${var.secret_key}"
   region     = "${var.region}"
 }
+
 resource "aws_instance" "ebizon" {
         ami = "${var.ami}"
         instance_type = "${var.instance_type}"
